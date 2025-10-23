f1c10eb7d10f044d2eaf49bf79d87c0353b6fffb
Danibro99 <shaheershaz2004@gmail.com>
2025-10-23 11:20:33 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
